3a1640ed05676c20938ee0bb378e36288c344e1f
Danilo Mello <danilodemellow@gmail.com>
2022-04-19 22:34:22 -0300

    feat: add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,37 @@
 import { useEffect, useState } from 'react';
-import { accessToken, logout } from './spotify';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+import { accessToken, logout, getCurrentUserProfile } from './spotify';
+import { catchErrors } from './utils';
 import './App.css';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
+
+    const fetchData = async () => {
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+    };
+
+    catchErrors(fetchData());
   }, []);
 
   return (
@@ -21,10 +46,37 @@ function App() {
             Entrar com Spotify
           </a>
         ) : (
-          <>
-            <h1>Entrou</h1>
-            <button onClick={logout}>Sair</button>
-          </>
+          <Router>
+            <ScrollToTop />
+            <Switch>
+              <Route path='/top-artists'>
+                <h1>Top Artistas</h1>
+              </Route>
+              <Route path='/top-tracks'>
+                <h1>Top Tracks</h1>
+              </Route>
+              <Route path='/playlists/:id'>
+                <h1>Playlist</h1>
+              </Route>
+              <Route path='/playlists'>
+                <h1>Playlists</h1>
+              </Route>
+              <Route path='/'>
+                <h1>Entrou</h1>
+                <button onClick={logout}>Sair</button>
+
+                {profile && (
+                  <>
+                    <h1>{profile.display_name}</h1>
+                    {profile.images.length && profile.images[0].url && (
+                      <img src={profile.images[0].url} alt='avatar' />
+                    )}
+                    <p>{profile.followers.total} Seguidores</p>
+                  </>
+                )}
+              </Route>
+            </Switch>
+          </Router>
         )}
       </header>
     </div>
