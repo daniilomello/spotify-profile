1771a446bfd488788d1e080faca6b4260eddda56
Danilo Mello <danilodemellow@gmail.com>
2022-04-19 21:36:20 -0300

    feat: add login with spotify feature on de client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { accessToken, logout } from './spotify';
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    setToken(accessToken);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        {!token ? (
+          <a
+            className='App-link'
+            href='http://localhost:8888/login'
+            rel='noopener noreferrer'
+          >
+            Entrar com Spotify
+          </a>
+        ) : (
+          <>
+            <h1>Entrou</h1>
+            <button onClick={logout}>Sair</button>
+          </>
+        )}
       </header>
     </div>
   );
