3c632246fc8237f69f091108d0126ef27fedf283
Danilo Mello <danilodemellow@gmail.com>
2022-04-23 18:18:01 -0300

    refactor: renaming pages files

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,6 @@
-export { default as Login } from './Login';
-export { default as Profile } from './Profile';
-export { default as TopArtists } from './TopArtists';
-export { default as TopTracks } from './TopTracks';
-export { default as Playlists } from './Playlists';
-export { default as Playlist } from './Playlist';
+export { default as Login } from './login.page';
+export { default as Profile } from './profile.page';
+export { default as TopArtists } from './top-artists.page';
+export { default as TopTracks } from './top-tracks.page';
+export { default as Playlists } from './playlists.page';
+export { default as Playlist } from './playlist.page';
